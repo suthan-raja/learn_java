32d9704681c7ddcd5f60eeebbddc6d6337c2b1e9
mspeagle <msp.philsys@gmail.com>
2024-10-16 17:28:24 +0530

    Selection sort completed

---
+++ src/main/java/msp/test/learn/algorithm/sort/SelectionSort.java
@@ -0,0 +1,35 @@
+package msp.test.learn.algorithm.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void selectionSort(int arr[]){
+
+        int n = arr.length;
+
+        for( int i = 0 ; i < n - 1 ; i++){
+
+            int minIndex = i;
+
+            for(int j = i + 1 ; j < n ; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("Selection Sorted : "+ Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+
+        int array[] = {64,34,25,12,22};
+        selectionSort(array);
+    }
+
+}
